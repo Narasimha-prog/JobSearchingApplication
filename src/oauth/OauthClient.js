e823c548735fab593eae2c043bab1e0c9e43804a
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-01-08 20:03:20 +0530

    deplaoy in vercel test 2 with call back url

--- src/oauth/OauthClient.js
+++ src/oauth/OauthClient.js
@@ -5,8 +5,8 @@ import { UserManager } from "oidc-client-ts";
 export const userManager = new UserManager({
   authority: "https://accounts.google.com",
   client_id: "616346721814-cfags13k4n37shqe4ku9i174i3db835a.apps.googleusercontent.com",
-  redirect_uri: "http://localhost:5174/JobSearchingApplication/callback",
+  redirect_uri: "https://job-searching-application.vercel.app/callback",
   response_type: "code",
   scope: "openid profile email",
-  post_logout_redirect_uri: "http://localhost:5174/JobSearchingApplication",
+  post_logout_redirect_uri: "https://job-searching-application.vercel.app",
 });
